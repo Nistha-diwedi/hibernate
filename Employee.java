74160df82af82c59abb05e394ac38243c3c1a4a6
Nistha Diwedi <118626112+Nistha-diwedi@users.noreply.github.com>
2024-03-03 23:00:49 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,21 @@
+import javax.persistence.Entity;
+
+@Entity
+public class Employee {
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
